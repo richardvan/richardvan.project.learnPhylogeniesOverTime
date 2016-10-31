630bde0180467fbd426e533986e88ca70b41b408
Richard Van <richardvan@Richards-MacBook-Air-2.local>
2016-10-30 21:48:48 -0700

    update to understand working with .jade templates; for loop working

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Expdress HE3LLo 3000 3' });
+  res.render('index', { title: 'Expdress HE3LLo 3000 3' ,
+											 	phylogenies: ["phylo1","phylo2","phlylo3"]
+											});
+	console.log("<< hello, it's me");
+	console.log("<< delta niner, delta niner, this is rave");
 });
 
 module.exports = router;
